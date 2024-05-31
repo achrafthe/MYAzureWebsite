597c3532b3ab4bcb2b4a19d1210b48a285dad927
achrafthe <160224430+achrafthe@users.noreply.github.com>
2024-05-31 11:02:11 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Initialize the map
+const map = L.map('map').setView([20, 0], 2);
+
+// Add OpenStreetMap tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// Load GeoJSON data
+fetch('countries.geojson')
+    .then(response => response.json())
+    .then(data => {
+        L.geoJSON(data, {
+            onEachFeature: (feature, layer) => {
+                layer.bindPopup(`<strong>${feature.properties.name}</strong><br>President: ${feature.properties.president}`);
+            }
+        }).addTo(map);
+    });
